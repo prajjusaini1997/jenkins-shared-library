3fc66b6b4172657c5b84ad5700b10dd639fc2b58
Prajjwal <prajjusaini1997@gmail.com>
2026-06-05 21:24:54 +0530

    Added kafka shared library code

--- vars/kafkaPipeline.groovy
+++ vars/kafkaPipeline.groovy
@@ -1,24 +1,27 @@
 def call(Map config = [:]) {
 
-    echo "===== Kafka Shared Library Started ====="
+    node {
 
-    stage('Clone Repo') {
-        git url: config.repoUrl, branch: config.branch ?: 'main'
+        echo "===== Kafka Shared Library Started ====="
+
+        stage('Clone Repo') {
+            git url: config.repoUrl, branch: config.branch ?: 'main'
+        }
+
+        stage('Approval') {
+            input message: 'Approve Kafka Deployment?'
+        }
+
+        stage('Playbook Execution') {
+            sh '''
+            echo "Running Ansible Playbook..."
+            '''
+        }
+
+        stage('Notification') {
+            echo "Sending Notification..."
+        }
+
+        echo "===== Kafka Shared Library Completed ====="
     }
-
-    stage('Approval') {
-        input message: 'Approve Kafka Deployment?'
-    }
-
-    stage('Playbook Execution') {
-        sh '''
-        echo "Running Ansible Playbook..."
-        '''
-    }
-
-    stage('Notification') {
-        echo "Sending Notification..."
-    }
-
-    echo "===== Kafka Shared Library Completed ====="
 }
